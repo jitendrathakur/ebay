45fc2bcb3bed82986166ee5b6393dc1c3bfccfb4
bolly <cis@CISM407.(none)>
2013-12-23 19:44:40 +0530

    added amazon feature

--- README.txt
+++ README.txt
@@ -1,3 +1,4 @@
 A Pen created at CodePen.io. You can find this one at http://codepen.io/anon/pen/dHKjE.
+Amazon Account Process: https://affiliate-program.amazon.com/gp/advertising/api/detail/main.html
 
  
--- post.php
+++ post.php
@@ -1,16 +1,14 @@
 <?php
-
+require 'lib/AmazonECS.class.php';
 $endpoint = 'http://svcs.ebay.com/services/search/FindingService/v1?';  
 $responseEncoding = 'XML';   
 $version = '1.8.0';   // API version number (they're actually up to 1.11.0 at this point
 $appID   = 'self56e11-173d-4020-8209-31afe61b5b6'; 
-$itemSort  = "EndTimeSoonest";
-$keywords = $_GET['keyword'];    // make sure this is a valid keyword or keywords
-
+$itemSort  = "EndTimeSoonest";?>
+<a href= "<?php echo "http://".$_SERVER['HTTP_HOST'].'/ebay/';?>">Home</a>
+<?php $keywords = $_GET['keyword'];    // make sure this is a valid keyword or keywords
 if ($_GET['category'] == 'ebay') {
-
 	echo "<h2>Ebay Result</h2>";
-
 	//find items advanced
 	$apicalla  = "http://svcs.ebay.com/services/search/FindingService/v1?SECURITY-APPNAME=self56e11-173d-4020-8209-31afe61b5b6&OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.0.0&RESPONSE-DATA-FORMAT=JSON&callback=_cb_findItemsByKeywords&REST-PAYLOAD&keywords=".$keywords."%203g&paginationInput.entriesPerPage=3";
 	    
@@ -21,6 +19,55 @@ if ($_GET['category'] == 'ebay') {
 	preg_match($rx_range_sec, $resp, $range_sec);
 	echo "<pre>";
 	print_r($range_sec[2]);
+
+}
+elseif($_GET['category'] == 'amazon')
+{
+echo "<h2>Amazon Result</h2>";
+$client = new AmazonECS('AKIAI47LUW5JKMASQSXQ', 'GXiY0kddNORa/1u+xcuwQg6PtEbD4CWzOQR8Vp/p', 'com', 'ASSOCIATE TAG');
+$client->setReturnType(AmazonECS::RETURN_TYPE_ARRAY);
+$response  = $client->category("All")->search($keywords);
+
+if (isset($response['Items']['Item']) ) {
+
+    //loop through each item
+    foreach ($response['Items']['Item'] as $result) {
+
+        //check that there is a ASIN code - for some reason, some items are not
+        //correctly listed. Im sure there is a reason for it, need to check.
+        if (isset($result['ASIN'])) {
+
+            //store the ASIN code in case we need it
+            $asin = $result['ASIN'];
+
+            //check that there is a URL. If not - no need to bother showing
+            //this one as we only want linkable items
+            if (isset($result['DetailPageURL'])) {
+
+                //set up a container for the details - this could be a DIV
+                echo "<p style='". IE_BACKGROUND . ";min-height: 60px; font-size: 90%;'>";
+
+                //create the URL link
+                echo "<a target='_Blank' href='" . $result['DetailPageURL'] ."'>";
+
+                //if there is a small image - show it
+                if (isset($result['SmallImage']['URL'] )) {
+                    echo "<img class='shadow' style=' margin: 0px; margin-left: 10px; border: 1px solid black; max-height: 55px;' align='right' src='". $result['SmallImage']['URL'] ."'>";
+                }
+
+                // if there is a title - show it
+                if (isset($result['ItemAttributes']['Title'])) {
+                    echo $result['ItemAttributes']['Title'] . "<br/>";
+                }
+
+                //close the paragraph
+                echo "</p>";
+
+            }
+        }
+    }
+
+} 
 
 }
 
